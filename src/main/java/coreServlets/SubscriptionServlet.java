ccac0244286874473e8d4e8f8fb8fefc52b1c0fd
FrostingNL <frostingnl@gmail.com>
2014-05-28 09:56:34 +0200

    subscriptionszien

--- src/main/java/coreServlets/SubscriptionServlet.java
+++ src/main/java/coreServlets/SubscriptionServlet.java
@@ -7,6 +7,9 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Properties;
 
 import javax.servlet.ServletException;
@@ -16,55 +19,54 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class SubscriptionServlet extends HttpServlet {
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.setContentType("text/html");
-		String id = getInfo("id", request, response);
-		request.setAttribute("id", id);
-		
-		String startDate = getInfo("startingdata", request, response);
-		request.setAttribute("startDate", startDate);
-		
-		String endDate = getInfo("endingdate", request, response);
-		request.setAttribute("endDate", endDate);
-		
-		String premium = getInfo("premium", request, response);
-		request.setAttribute("premium", premium);
-		request.getRequestDispatcher("/subscriptions.jsp").forward(request, response);
-	}
-	
-	public String getInfo(String info, HttpServletRequest request, HttpServletResponse response){
 		HttpSession s = request.getSession();
-		int id = (Integer)s.getAttribute("Logged");
-		String foundInfo = "Niet gevonden!";
+		int idint = (Integer) s.getAttribute("Logged");
+		int count = 0;
 		try {
 			Class.forName("org.postgresql.Driver");
 		} catch (ClassNotFoundException e1) {
 			e1.printStackTrace();
 		}
 		Properties prop = new Properties();
-		 String path = "res/dbprops.txt";
-		 try{
-		 prop.load(new FileInputStream(getServletContext().getRealPath(path)));}catch(Exception e){e.printStackTrace();}
-		 String user = prop.getProperty("username");
-		 String pass1 = prop.getProperty("pass");
-		 String host = prop.getProperty("host");
-		 String port = prop.getProperty("port");
-		 String dbname = prop.getProperty("dbname");
-		 String url = "jdbc:postgresql://" + host + ":" + port + "/" + dbname;
+		String path = "res/dbprops.txt";
+		try {
+			prop.load(new FileInputStream(getServletContext().getRealPath(path)));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		String user = prop.getProperty("username");
+		String pass1 = prop.getProperty("pass");
+		String host = prop.getProperty("host");
+		String port = prop.getProperty("port");
+		String dbname = prop.getProperty("dbname");
+		String url = "jdbc:postgresql://" + host + ":" + port + "/" + dbname;
 		try (Connection conn = DriverManager.getConnection(url, user, pass1)) {
 			try (Statement stmt = conn.createStatement();
 					ResultSet rs = stmt
-							.executeQuery("SELECT " + info + " FROM subscription s, pays_a p "+ 
-									 "WHERE p.customer=' "+ id + 
-									 "WHERE s.id = p.subscription';")) {
-				if(!rs.next()) {
-					return foundInfo;
+							.executeQuery("SELECT * FROM subscription s, pays_a p "
+									+ "WHERE p.customer= '"
+									+ idint
+									+ "'"
+									+ " AND s.id = p.subscription;")) {
+				while (rs.next()) {
+					request.setAttribute("id" + "" +Integer.toString(count), rs.getString("id"));
+					request.setAttribute("start" + "" +Integer.toString(count), rs.getDate("startingdata"));
+					request.setAttribute("eind" + "" +Integer.toString(count), rs.getDate("endingdate"));
+					request.setAttribute("premium" + "" +Integer.toString(count), rs.getBoolean("premium"));
+					System.out.println("id"+ Integer.toString(count));
+					count++;
+					request.setAttribute("rowcount", count);
 				}
-				 foundInfo = rs.getString(info);
-				}
-		} catch (SQLException e1) {
-			e1.printStackTrace();
-		}
-		return foundInfo;
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		request.getRequestDispatcher("/subscriptions.jsp").forward(request, response);
 	}
 }
