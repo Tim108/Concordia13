eaabf89049e69d312c41f8457867deb84cd3afe6
AeroKevNL <kevin1226@live.nl>
2014-06-03 21:17:50 +0200

    Stuff

--- src/main/java/coreServlets/ArtServlet.java
+++ src/main/java/coreServlets/ArtServlet.java
@@ -1,7 +1,15 @@
 package coreServlets;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -16,6 +24,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
 
 @MultipartConfig
 public class ArtServlet extends HttpServlet {
@@ -34,11 +43,45 @@ public class ArtServlet extends HttpServlet {
 		String heigth = (String)request.getAttribute("heigth");
 		String technique = (String)request.getAttribute("technique");
 		String orientation = (String)request.getAttribute("orientation");
-		String file = (String)request.getAttribute("file");
-
-		System.out.println(file);
 		
-		try {
+		final Part filePart = request.getPart("file");
+	    final String fileName = getFileName(filePart);
+
+	    OutputStream out = null;
+	    InputStream filecontent = null;
+	    final PrintWriter writer = response.getWriter();
+	    File f = null;
+	    try {
+	    	f = new File(fileName);
+	    	out = new FileOutputStream(f);
+	        filecontent = filePart.getInputStream();
+
+	        int read = 0;
+	        final byte[] bytes = new byte[1024];
+
+	        while ((read = filecontent.read(bytes)) != -1) {
+	            out.write(bytes, 0, read);
+	        }
+	        writer.println("New file " + fileName + " created at " + f.getCanonicalPath());
+	    } catch (FileNotFoundException fne) {
+	        writer.println("You either did not specify a file to upload or are "
+	                + "trying to upload a file to a protected or nonexistent "
+	                + "location.");
+	        writer.println("<br/> ERROR: " + fne.getMessage());
+	    } finally {
+	        if (out != null) {
+	        	System.out.println(f.getAbsolutePath());
+	            out.close();
+	        }
+	        if (filecontent != null) {
+	            filecontent.close();
+	        }
+	        if (writer != null) {
+	            writer.close();
+	        }
+	    }
+		
+		/*try {
 			Class.forName("org.postgresql.Driver");
 		} catch (ClassNotFoundException e1) {
 			e1.printStackTrace();
@@ -55,13 +98,24 @@ public class ArtServlet extends HttpServlet {
 		try (Connection conn = DriverManager.getConnection(url, user, pass1)) {
 			try (PreparedStatement ps = conn.prepareStatement("INSERT INTO Art (name, source) VALUES(?,?);")){
 				ps.setString(1, title);
-				ps.setString(2, file);
+				//ps.setString(2, file);
 			}
 			try (PreparedStatement ps = conn.prepareStatement("INSERT INTO Artpiece (artist, heigth, width, style, technique, orientation, price, rating) VALUES (????????);")) {
 				
 			}
 		} catch (SQLException e1) {
 			e1.printStackTrace();
-		}
+		}*/
+	}
+	
+	private String getFileName(final Part part) {
+	    final String partHeader = part.getHeader("content-disposition");
+	    for (String content : part.getHeader("content-disposition").split(";")) {
+	        if (content.trim().startsWith("filename")) {
+	            return content.substring(
+	                    content.indexOf('=') + 1).trim().replace("\"", "");
+	        }
+	    }
+	    return null;
 	}
 }
