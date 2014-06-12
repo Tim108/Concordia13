8e718afc2339e90988de94c708b1f96b70c33985
Tim108 <tsonderen108@gmail.com>
2014-06-12 11:52:45 +0200

    test shit

---
+++ src/test/java/coreTests/Register.java
@@ -0,0 +1,39 @@
+package coreTests;
+
+import org.junit.Test;
+
+import static net.sourceforge.jwebunit.junit.JWebUnit.*;
+import net.sourceforge.jwebunit.util.TestingEngineRegistry;
+
+import org.junit.Before;
+
+
+public class Register {
+
+	@Before
+	public void prepare() {
+		System.out.println("a");
+		setBaseUrl("localhost:8080/concordia/");
+		System.out.println("b");
+	}
+
+	@Test
+	public void testRegister() {
+		System.out.println("0");
+		beginAt("/register");
+		System.out.println("1");
+		assertTitleEquals("Registreren");
+		setTextField("name", "Testy");
+		setTextField("surname", "Tester");
+		setTextField("address", "Teststreet");
+		setTextField("housenumber", "8");
+		setTextField("city", "Testercity");
+		setTextField("postal", "1122AB");
+		setTextField("phone", "1122334455");
+		setTextField("email", "testy@testserver.com");
+		setTextField("pass", "notpassword");
+		System.out.println("2");
+		submit();
+		assertTitleEquals("Populaire werken");
+	}
+}
