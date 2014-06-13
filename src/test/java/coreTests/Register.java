a186d5b580d9def151906a93fec840209475b164
AeroKevNL <kevin1226@live.nl>
2014-06-13 10:06:16 +0200

    RegisterTest

--- src/test/java/coreTests/Register.java
+++ src/test/java/coreTests/Register.java
@@ -22,18 +22,30 @@ public class Register {
 		System.out.println("0");
 		beginAt("/register.jsp");
 		System.out.println("1");
-		//assertTitleEquals("Registreren");
+		assertFormPresent();
+		assertFormElementPresent("name");
 		setTextField("name", "Testy");
+		assertFormElementPresent("surname");
 		setTextField("surname", "Tester");
+		assertFormElementPresent("address");
 		setTextField("address", "Teststreet");
+		assertFormElementPresent("housenumber");
 		setTextField("housenumber", "8");
+		assertFormElementPresent("city");
 		setTextField("city", "Testercity");
+		assertFormElementPresent("postal");
 		setTextField("postal", "1122AB");
+		assertFormElementPresent("phone");
 		setTextField("phone", "1122334455");
+		assertFormElementPresent("email");
 		setTextField("email", "testy@testserver.com");
+		assertFormElementPresent("pass");
 		setTextField("pass", "notpassword");
+		assertFormElementPresent("newsletter");
+		assertCheckboxSelected("newsletter");
+		uncheckCheckbox("newsletter");
+		assertCheckboxNotSelected("newsletter");
 		System.out.println("2");
 		submit();
-		assertTitleEquals("Populaire werken");
 	}
 }
