2518d2c63b3feea4300fe225bc0f4970774eaae5
Tim108 <tsonderen108@gmail.com>
2014-06-13 11:15:09 +0200

    login test

--- src/test/java/coreTests/Login.java
+++ src/test/java/coreTests/Login.java
@@ -12,7 +12,15 @@ public class Login {
 	}
 
 	@Test
-	public void testUser() {
-		JWebUnit.clickButton("acc");
+	public void testLogin() {
+		System.out.println("0");
+		beginAt("");
+		System.out.println("a");
+		assertElementPresent("acc");
+		clickElementByXPath("//*[@id=\"acc\"]");
+		setTextField("email", "testy@testserver.com");
+		setTextField("password", "notpassword");
+		submit();
+		JWebUnit.assertLinkPresent("Testy Tester");
 	}
 }
