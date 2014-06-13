659e68b3e63ef7947960463b51dac52f9740b30a
Tim108 <tsonderen108@gmail.com>
2014-06-13 10:40:11 +0200

    login test

---
+++ src/test/java/coreTests/Login.java
@@ -0,0 +1,18 @@
+package coreTests;
+
+import static net.sourceforge.jwebunit.junit.JWebUnit.*;
+import net.sourceforge.jwebunit.junit.JWebUnit;
+import org.junit.*;
+
+public class Login {
+	
+	@Before
+	public void prepare() {
+		setBaseUrl("http://localhost:8080/concordia");
+	}
+
+	@Test
+	public void testUser() {
+		JWebUnit.clickButton("acc");
+	}
+}
